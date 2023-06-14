f6a6963e43440f0642aea337862920ef00734467
Loser <tacobellcommercial@gmail.com>
2023-06-14 09:11:26 -0500

    make it great

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Navbar(){
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/home">React Weather</a>
+            <Link className="navbar-brand" to="/home">React Weather</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
